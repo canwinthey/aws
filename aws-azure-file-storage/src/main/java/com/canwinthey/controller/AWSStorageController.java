157ca737eb477aefb5b221b70dffc1933906c4f1
canwinthey <83583191+canwinthey@users.noreply.github.com>
2023-05-26 12:04:19 +0530

    AWS/Azure file upload/download
    AWS S3 file upload/download

---
+++ aws-azure-file-storage/src/main/java/com/canwinthey/controller/AWSStorageController.java
@@ -0,0 +1,40 @@
+package com.canwinthey.controller;
+
+import com.canwinthey.service.AWSStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/aws/file")
+public class AWSStorageController {
+
+    @Autowired
+    private AWSStorageService service;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file) {
+        System.out.println("uploadFile()..");
+        return new ResponseEntity<>(service.uploadFile(file), HttpStatus.OK);
+    }
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
+        byte[] data = service.downloadFile(fileName);
+        ByteArrayResource resource = new ByteArrayResource(data);
+        return ResponseEntity
+                .ok()
+                .contentLength(data.length)
+                .header("Content-type", "application/octet-stream")
+                .header("Content-disposition", "attachment; filename=\"" + fileName + "\"")
+                .body(resource);
+    }
+
+    @DeleteMapping("/delete/{fileName}")
+    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
+        return new ResponseEntity<>(service.deleteFile(fileName), HttpStatus.OK);
+    }
+}
